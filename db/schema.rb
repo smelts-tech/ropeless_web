ef862e3ee4f6daab0f763d48b40f44ad51f7b3d3
Rob Sterner <fermion@github.com>
2020-08-06 22:25:21 -0400

    adds users.full_name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_022025) do
+ActiveRecord::Schema.define(version: 2020_08_07_022437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2020_08_07_022025) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "role", default: "fisher"
     t.string "permit"
+    t.string "full_name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["permit"], name: "index_users_on_permit", unique: true
