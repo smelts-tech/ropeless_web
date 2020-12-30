ff42d9ebdabffa160cdbd15dc75b7b062961d0d1
ryan <ryan@everquote.com>
2020-12-30 09:08:40 -0500

    View events for a device.

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -2,5 +2,6 @@ class DevicesController < ApplicationController
   def show
     @device = Device.find(params[:id])
     @fisher = Fisher.find(@device.fisher_id)
+    @ordered_event_list = Device.where(:modem_id => @device.modem_id).order(dt: :desc)
   end
 end
