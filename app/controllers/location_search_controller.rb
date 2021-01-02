7292ad7e3d3cc591f4d52317802ae316cf5243a0
ryan <ryan@everquote.com>
2021-01-02 13:02:25 -0500

    Location search implemented.  No tests.

---
+++ app/controllers/location_search_controller.rb
@@ -0,0 +1,22 @@
+class LocationSearchController < ApplicationController
+  before_action :validate_params, only: [:create, :index]
+
+  def create
+    redirect_to location_search_path(request.parameters)
+  end
+
+  def index
+    meters = params[:radius].to_f*1609.34 # meters per mile
+    @devices = Device.unique.where("ST_DWithin(geom, ST_MakePoint(#{params[:longitude]},#{params[:latitude]})::geography, #{meters})")
+  end
+
+  private
+
+  def validate_params
+    #@location = Location.new(params)
+    #if !@location.valid?
+    #  render json: { error: @location.errors } and return
+    #end
+  end
+
+end
