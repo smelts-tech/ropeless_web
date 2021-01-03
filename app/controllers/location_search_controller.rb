dec7e42885baf33335a05d760d857160cf422281
ryan <ryan@everquote.com>
2021-01-02 22:09:22 -0500

    Hey, we've got a working map with markers!

--- app/controllers/location_search_controller.rb
+++ app/controllers/location_search_controller.rb
@@ -6,5 +6,8 @@ class LocationSearchController < ApplicationController
   def index
     meters = params[:radius].to_f * 1609.34 # meters per mile
     @devices = Device.unique.where("ST_DWithin(geom, ST_MakePoint(#{params[:longitude]},#{params[:latitude]})::geography, #{meters})")
+    @unique_devices = Device.unique.collect do |device|
+        [ device.geom.latitude, device.geom.longitude, device.modem_id ]
+    end
   end
 end
--- app/javascript/packs/location_search.js
+++ app/javascript/packs/location_search.js
@@ -1,8 +1,29 @@
-function initMap(lat, lng) {
+window.initMap = function (lat, lng, markers) {
     var myCoords = new google.maps.LatLng(lat, lng);
     var mapOptions = {
         center: myCoords,
-        zoom: 14
+        zoom: 14,
+        mapTypeId: 'satellite'
     };
     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    // markers
+    for (var i = 0; i < markers.length; i++) {
+        lat = markers[i][0];
+        lng = markers[i][1];
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(lat, lng),
+            map: map
+        });
+        // process multiple info windows
+        (function(marker, i) {
+            // add click event
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow = new google.maps.InfoWindow({
+                    content: 'Modem ID:' + markers[i][2]
+                });
+                infowindow.open(map, marker);
+            });
+        })(marker, i);
+    }
 }
