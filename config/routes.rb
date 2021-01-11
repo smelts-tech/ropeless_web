fa083fe56791eed23dec98bb969fe12056d795b2
Rob Sterner <fermion@github.com>
2021-01-11 12:36:09 -0500

    move around devise files, configure some controllers

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   resources :device_uploads, controller: "device_upload"
   resources :location_search, controller: "location_search"
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords'
+  }
 
   resource :dashboard, only: :show, controller: "dashboard"
 
