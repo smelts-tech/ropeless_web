0e40ce1975f63d27e737e1e42c7141ec4265bab5
Rob Sterner <fermion@github.com>
2021-01-12 18:56:45 -0500

    simple Stimulus registrations controller

---
+++ app/javascript/controllers/registrations_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from 'stimulus';
+
+export default class extends Controller {
+
+  static targets = [ "accessNeeded" ]
+
+  connect() {
+    this.userTypeChanged()
+  }
+
+  userTypeChanged() {
+    const value = this.accessNeededTarget.value;
+    document.querySelectorAll('[data-show-if]').forEach((element) => {
+      if(element.getAttribute('data-show-if') === value) {
+        element.removeAttribute('hidden')
+      } else {
+        element.setAttribute('hidden', 'hidden')
+      }
+    })
+  }
+}
