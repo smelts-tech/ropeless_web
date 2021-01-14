74a295ef0661130499a9065d0eb307c26304f04c
Rob Sterner <fermion@github.com>
2021-01-13 20:09:19 -0500

    put missing address fields into the form, allow them through devise

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,7 +42,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:access_needed, :full_name, :permit_number, :address, :phone_number])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:access_needed, :full_name, :permit_number, :address, :city, :state, :zip_code, :phone_number])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
