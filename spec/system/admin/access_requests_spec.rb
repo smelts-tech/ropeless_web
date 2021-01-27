d5b43d882600c6eef0988c9f25a9c290f658e279
Rob Sterner <fermion@github.com>
2021-01-26 21:26:00 -0500

    move this spec as well

--- spec/system/admin/access_requests_spec.rb
+++ spec/system/admin/access_requests_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Access Requests", type: :system do
+RSpec.describe "Admin > Access Requests", type: :system do
   before do
     driven_by(:rack_test)
   end
