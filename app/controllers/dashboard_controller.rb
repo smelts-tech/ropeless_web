496612a00dbc0c0edd04f095561c1bbbe2f5ffae
Rob Sterner <fermion@github.com>
2021-04-02 12:49:39 -0400

    fisherifying some more things

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,12 @@
 class DashboardController < ApplicationController
   def show
     @fishers = Fisher.includes(:devices).all
-    @devices = Device.includes(:fisher).all
+    @devices = Device.includes(:fisher)
+
+    if current_user_fisher?
+      @devices = @devices.where(fisher: current_user)
+    end
+
+    @devices = @devices.all
   end
 end
